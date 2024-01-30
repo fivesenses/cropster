660fe2d16d310ef17aa6ad9a63c0dc9f81a5a7f1
patrickreillydev <79639852+patrickreillydev@users.noreply.github.com>
2024-01-31 03:13:17 +1100

    Update physical_sheets.rb

--- lib/cropster/response/physical_sheets.rb
+++ lib/cropster/response/physical_sheets.rb
@@ -3,7 +3,7 @@
 #
 module Cropster::Response
   class PhysicalSheets < Cropster::Response::FormattedResponseItem
-    attr_accessor  :name, :is_density_calculated, :is_global, :is_parchment_coffee, :is_peaberry_screen_size_enabled, :languages, :lock_state, :screen_size_mode
+    attr_accessor  :name, :is_density_calculated, :is_global, :is_parchment_coffee, :is_peaberry_screen_size_enabled, :languages, :lock_state, :screen_size_mode, :defects_path
 
     def load_attributes(attributes)
       return if attributes.nil?
@@ -16,29 +16,19 @@ module Cropster::Response
       @lock_state = attributes[:lockState]
       @screen_size_mode = attributes[:screenSizeMode]
     end
+
+    def load_from_data(data)
+      super
+      @defects_path = ""
+      load_path(data[:relationships][:physicalSheetDefects])
+
+    end
+
+    def load_path(item)
+      return if item.nil?
+      @defects_path = item[:links][:related]
+    end
+
   end
 end
   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
