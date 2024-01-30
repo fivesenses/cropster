7e66408d95db514e21722f4deb9b6575cc215573
patrickreillydev <79639852+patrickreillydev@users.noreply.github.com>
2024-01-31 01:55:07 +1100

    include defects path for PhysicalResults

--- lib/cropster/response/physical_results.rb
+++ lib/cropster/response/physical_results.rb
@@ -3,7 +3,7 @@
 #
 module Cropster::Response
   class PhysicalResults < Cropster::Response::FormattedResponseItem
-    attr_accessor :remote_id, :id_tag,:category,:color_overall,:color_uniformity,:comment,:density,:evaluation_date,:evaluator,:lab,:peaberry_screen_sizes,:quaker_count,:screen_size_summary,:screen_sizes,:smell,:water_activity, :green_weight, :moisture, :parchment, :roasted_weight, :temperature, :density_volume, :density_weight, :milling_weight_difference
+    attr_accessor :remote_id, :id_tag,:category,:color_overall,:color_uniformity,:comment,:density,:evaluation_date,:evaluator,:lab,:peaberry_screen_sizes,:quaker_count,:screen_size_summary,:screen_sizes,:smell,:water_activity, :green_weight, :moisture, :parchment, :roasted_weight, :temperature, :density_volume, :density_weight, :milling_weight_difference, :defects_path
 
 
     def load_attributes(attributes)
@@ -27,12 +27,25 @@ module Cropster::Response
       @milling_weight_difference = load_weight(attributes[:millingWeightDifference])
       @lab = attributes [:lab]
       @peaberry_screen_sizes = attributes [:peaberryScreenSizes]
-      @quaker_count = attributes [:quakerCount]
-      @screen_size_summary = attributes [:screenSizeSummary]
-      @screen_sizes = attributes [:screenSizes]
-      @smell = attributes [:smell]
-      @water_activity = attributes [:waterActivity]    
+      @quaker_count = attributes[:quakerCount]
+      @screen_size_summary = attributes[:screenSizeSummary]
+      @screen_sizes = attributes[:screenSizes]
+      @smell = attributes[:smell]
+      @water_activity = attributes[:waterActivity]    
     end
+
+    def load_from_data(data)
+      super
+      @defects_path = ""
+      load_path(data[:relationships][:sensorialSheetItems])
+
+    end
+
+    def load_path(item)
+      return if item.nil?
+      @defects_path = item[:links][:related]
+    end
+
   end
 end
   
