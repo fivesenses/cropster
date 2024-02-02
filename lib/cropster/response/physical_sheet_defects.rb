6a9c2be4bba0440eb4f17f35f05510e4baff734a
patrickreillydev <79639852+patrickreillydev@users.noreply.github.com>
2024-02-02 23:54:46 +1100

    Update physical_sheet_defects.rb

--- lib/cropster/response/physical_sheet_defects.rb
+++ lib/cropster/response/physical_sheet_defects.rb
@@ -3,20 +3,18 @@
 #
 module Cropster::Response
   class PhysicalSheetDefects < Cropster::Response::FormattedResponseItem
-    attr_accessor  :name, :formula, :measurement_type, :position, 
-    :sheet_group, 
-    :physical_sheet_id,
+    attr_accessor  :name, :formula, :measurement_type, :position, :sheet_group, :physical_sheet_id,
 
 
     def load_from_data(data)
+      super(data)
       puts "dataa #{data}"
-      load_parent_data(data[:relationships][:physicalSheet])
     end
 
-    def load_parent_data(parent)
-      return if parent.nil?
-      @physical_sheet_id = load_parent(parent[:data])
-    end
+    # def load_parent_data(parent)
+    #   return if parent.nil?
+    #   @physical_sheet_id = load_parent(parent[:data])
+    # end
 
 
     def load_attributes(attributes)
