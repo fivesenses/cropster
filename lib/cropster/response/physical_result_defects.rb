9741069949e44f652908eb1fb73748a57a6de144
patrickreillydev <79639852+patrickreillydev@users.noreply.github.com>
2024-02-04 18:11:33 +1100

    Update physical_result_defects.rb

--- lib/cropster/response/physical_result_defects.rb
+++ lib/cropster/response/physical_result_defects.rb
@@ -3,9 +3,23 @@
 #
 module Cropster::Response
   class PhysicalResultDefects < Cropster::Response::FormattedResponseItem
-    attr_accessor  :calculated_value, :count, :weight
+    attr_accessor  :calculated_value, :count, :weight, :physical_result, :physical_sheet_defect
 
+    def load_from_data(data)
+      super
+      @physical_result = ""
+      @physical_sheet_defect = ""
+      get_parent(data[:relationships][:physicalResult])
+      get_sheet_defect(data[:relationships][:physicalSheetDefect])
+    end
 
+    def get_parent(item)
+      @physical_result = load_parent(item[:data])
+    end
+
+    def get_sheet_defect(item)
+      @physical_sheet_defect = load_parent(item[:data])
+    end
 
     def load_attributes(attributes)
       return if attributes.nil?
