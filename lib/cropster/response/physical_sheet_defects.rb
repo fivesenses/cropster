ed4c29fb1be4adb321a3549c114f874974e18ba6
patrickreillydev <79639852+patrickreillydev@users.noreply.github.com>
2024-02-04 16:47:00 +1100

    Update physical_sheet_defects.rb

--- lib/cropster/response/physical_sheet_defects.rb
+++ lib/cropster/response/physical_sheet_defects.rb
@@ -3,17 +3,22 @@
 #
 module Cropster::Response
   class PhysicalSheetDefects < Cropster::Response::FormattedResponseItem
-    attr_accessor  :name, :formula, :measurement_type, :position, :sheet_group, :physical_sheet
+    attr_accessor  :name, :formula, :measurement_type, :position, :sheet_group, :physical_sheet_parent
 
 
     def load_from_data(data)
       super
 
-      puts "PhysicalSheetDefects LOADING DATA #{data}"
+      puts "Start PhysicalSheetDefects LOADING DATA #{data}"
+      puts "relationships #{data[:relationships]}"
+      puts "physicalSheet #{data[:relationships][:physicalSheet]}"
+      puts "physicalSheet data #{data[:relationships][:physicalSheet][:data]}"
+      puts "physicalSheet data id #{data[:relationships][:physicalSheet][:data][:id]}"
 
-      @phyiscal_sheet = ''
-      @phyiscal_sheet = data[:relationships][:physicalSheet][:data][:id]
-      
+      @physical_sheet_parent = ''
+      @physical_sheet_parent = data[:relationships][:physicalSheet][:data][:id]
+      puts @physical_sheet_parent
+      puts " end PhysicalSheetDefects"
     end
 
     def load_attributes(attributes)
